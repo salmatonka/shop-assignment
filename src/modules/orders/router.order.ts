79e988c2cad7862c65588ff347cf63a8658292a3
ommes salma <ommes918@gmail.com>
2024-05-30 12:13:59 +0600

    order finnal commit

--- src/modules/orders/router.order.ts
+++ src/modules/orders/router.order.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post("/", OrderControllers.createOrder);
 router.get("/", OrderControllers.getAllOrders);
+router.get("/:orderId", OrderControllers.deleteProduct);
 
 export const OrderRoutes = router;
